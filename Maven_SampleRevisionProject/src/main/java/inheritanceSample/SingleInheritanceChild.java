804af43c54ce6cb977b8f50999901c6125e9ac07
rubyv <rubyv@LENOVO>
2023-04-11 14:05:54 +0530

    First Commit

---
+++ Maven_SampleRevisionProject/src/main/java/inheritanceSample/SingleInheritanceChild.java
@@ -0,0 +1,15 @@
+package inheritanceSample;
+
+public class SingleInheritanceChild extends SingleInheritanceSample{
+	public void print() {
+		System.out.println("This is child class");
+	}
+
+	public static void main(String[] args) {
+		SingleInheritanceChild ob= new SingleInheritanceChild();
+		ob.display();
+		ob.print();
+
+	}
+
+}
---
+++ Maven_SampleRevisionProject/src/main/java/inheritanceSample/SingleInheritanceSample.java
@@ -0,0 +1,9 @@
+package inheritanceSample;
+
+public class SingleInheritanceSample {
+	public void display()
+	{
+		System.out.println("This is parent class");
+	}
+
+}
