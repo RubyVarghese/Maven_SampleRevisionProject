5c65d37781e968fd10d869c40723fbdb3b75db6f
rubyv <rubyv@LENOVO>
2023-04-11 22:00:48 +0530

    updated commit

--- Maven_SampleRevisionProject/src/main/java/inheritanceSample/SingleInheritanceChild.java
+++ Maven_SampleRevisionProject/src/main/java/inheritanceSample/SingleInheritanceChild.java
@@ -2,7 +2,7 @@ package inheritanceSample;
 
 public class SingleInheritanceChild extends SingleInheritanceSample {
 	public void print() {
-		System.out.println("This is child class");
+		System.out.println("This is child class derived from parent");
 	}
 
 	public static void main(String[] args) {
